66fec1a6b585cf4f9d46d464ba51706b8f152c0b
Helton Wernik <heltonwernik@gmail.com>
2018-09-27 02:56:28 +0300

    add files scanner

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@
 #@TODO: create optparce to target url
 
 import requests
+import time
+import sys
 
 def request(url):
     try:
@@ -9,16 +11,24 @@ def request(url):
     except requests.exceptions.ConnectionError:
         pass
 
-target_url = "google.com"
+target_url = ""
 
-with open("subdomains-wodlist.txt", "r") as wordlist_file:
+with open("wordlists/subdomains-wodlist.txt", "r") as wordlist_file:
     for line in wordlist_file:
         word = line.strip()
         test_url = word + "." + target_url
-        test_url2 = target_url + "/" + word
         response = request(test_url)
+        print("\r " + test_url + "         "),
+        sys.stdout.flush()
         if response:
-            print("[+] I found a subdomain --> " + test_url)
-        response2 = request(test_url2)
-        if response2:
-            print("[+] I found a subdomain --> " + test_url2)
+            print("[+] I found a subdomain")
+
+with open("wordlists/files-and-dirs-wordlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = target_url + "/" + word
+        response = request(test_url)
+        print("\r " + test_url + "         "),
+        sys.stdout.flush()
+        if response:
+            print("[+] I found a url")
